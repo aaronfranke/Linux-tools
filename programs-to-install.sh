893274c8107fe0953a6fabcfc94a520547623e21
Aaron Franke <arnfranke@yahoo.com>
2016-03-20 02:18:03 -0500

    Added Steam, Mono runtime stuff, and Screenfetch
    Also removed the software-center* (wildcard) entry.

--- programs-to-install.sh
+++ programs-to-install.sh
@@ -21,7 +21,13 @@ sudo apt install -y gksu pv ffmpeg libtcmalloc-minimal4 python-gpgme
 
 sudo apt install -y libdvd-pkg
 
+sudo apt install -y mono-complete mono-vbnc libopenal1
+
 sudo apt install -y gparted thunar youtube-dl gedit
+
+sudo apt install -y screenfetch
+
+sudo apt install -y cowsay fortune-mod
 
 sudo apt install -y openjdk-8-jre psensor
 
@@ -29,13 +35,15 @@ sudo apt install -y vlc
 
 sudo apt install -y gimp
 
-sudo apt install -y software-center software-center*
+sudo apt install -y software-center
 
 sudo apt install -y libreoffice
 
 sudo apt install -y wine playonlinux
 
 sudo update-ca-certificates -f
+
+sudo apt install -y steam
 
 sudo apt update
 
