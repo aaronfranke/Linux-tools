17b1c28ba1596d5b81ea353ba94ef3697fa15f00
Aaron Franke <arnfranke@yahoo.com>
2016-06-04 21:38:47 -0500

    Re-organization and added icoutils
    For most most part now, background stuff comes first, GUI stuff comes last.

--- programs-to-install.sh
+++ programs-to-install.sh
@@ -15,19 +15,17 @@ sudo apt full-upgrade -y
 
 sudo apt install -y vlc
 
-sudo apt install -y screenfetch 
+sudo apt install -y screenfetch
 
-sudo apt install -y gksu 
+sudo apt install -y gksu
 
 sudo apt install -y pv
 
-sudo apt install -y notepadqq
+sudo apt install -y icoutils
 
-sudo apt install -y grub-customizer 
+sudo apt install -y youtube-dl
 
-sudo apt install -y gnome-system-monitor
-
-sudo apt install -y ffmpeg  
+sudo apt install -y ffmpeg
 
 sudo apt install -y openssh-server
 
@@ -35,47 +33,61 @@ sudo apt install -y python-gpgme
 
 sudo apt install -y libtcmalloc-minimal4
 
-sudo apt install -y mono-complete mono-vbnc 
+sudo apt install -y lib32z1 
+
+sudo apt install -y lib32ncurses5 
+
+sudo apt install -y lib32stdc++6
 
 sudo apt install -y libopenal1
 
-sudo apt install -y gparted 
+sudo apt install -y mono-complete 
+
+sudo apt install -y mono-vbnc
+
+sudo apt install -y cmatrix
+
+sudo apt install -y cowsay
+
+sudo apt install -y fortune-mod
+
+sudo apt install -y openjdk-8-jre
+
+sudo apt install -y ubuntu-sso-client-qt
+
+sudo apt install -y software-center
+
+sudo apt install -y synaptic
+
+sudo apt install -y grub-customizer
+
+sudo apt install -y gparted
+
+sudo apt install -y dconf-editor
+
+sudo apt install -y gnome-system-monitor
+
+sudo apt install -y psensor 
 
 sudo apt install -y thunar 
 
 sudo apt install -y nautilus
 
-sudo apt install -y dconf-editor
+sudo apt install -y pcmanfm
+
+sudo apt install -y gedit
+
+sudo apt install -y notepadqq
 
 sudo apt install -y firefox
 
 sudo apt install -y chromium-browser
 
-sudo apt install -y youtube-dl 
-
-sudo apt install -y gedit
-
-sudo apt install -y cmatrix
-
-sudo apt install -y cowsay 
-
-sudo apt install -y fortune-mod
-
-sudo apt install -y openjdk-8-jre 
-
-sudo apt install -y psensor
-
-sudo apt install -y synaptic
-
-sudo apt install -y software-center
-
-sudo apt install -y ubuntu-sso-client-qt
-
 sudo apt install -y gimp
 
 sudo apt install -y libreoffice
 
-sudo apt install -y wine 
+sudo apt install -y wine
 
 sudo apt install -y playonlinux
 
@@ -94,6 +106,7 @@ sudo apt dist-upgrade -y
 sudo apt full-upgrade -y
 
 sudo rm -R /var/log
+
 
 
 
