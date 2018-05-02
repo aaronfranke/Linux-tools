06c284159681dba29567222f863eef2f35b14270
Aaron Franke <arnfranke@yahoo.com>
2018-05-01 19:03:17 -0500

    Update for Ubuntu 18.04 Bionic Beaver
    Update for Ubuntu 18.04 Bionic Beaver

--- ubuntu-only/programs-to-install.sh
+++ ubuntu-only/programs-to-install.sh
@@ -3,207 +3,135 @@
 
 sudo dpkg --add-architecture i386 
 
+# Do updates first 
+
 sudo apt update 
-
 sudo apt upgrade -y 
-
-sudo apt dist-upgrade -y 
-
 sudo apt full-upgrade -y 
 
-wget https://dl.winehq.org/wine-builds/Release.key 
+# Itch stuff 
 
-sudo apt-key add Release.key 
-
-sudo rm -f Release.key 
-
-ver=$(lsb_release -sr); if [ $ver != "16.10" -a $ver != "17.04" -a $ver != "16.04" ]; then ver=16.04; fi 
-
-echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list 
-
-wget -q http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/Release.key -O- | sudo apt-key add - 
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - 
-
-sudo apt-add-repository -y https://dl.winehq.org/wine-builds/ubuntu/ 
-
-sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer 
-
-sudo add-apt-repository -y ppa:notepadqq-team/notepadqq 
-
-sudo add-apt-repository -y ppa:webupd8team/java 
-
-curl https://dl.itch.ovh/archive.key | sudo apt-key add - 
-
+wget -O - https://dl.itch.ovh/archive.key | sudo apt-key add - 
 ITCHIO_DEB="deb https://dl.bintray.com/itchio/deb xenial main" 
-
 echo $ITCHIO_DEB | sudo tee /etc/apt/sources.list.d/itchio.list 
 
-wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+# Wine stuff 
 
-sudo dpkg -i discord.deb
+wget https://dl.winehq.org/wine-builds/Release.key 
+sudo apt-key add Release.key 
+sudo rm -f Release.key 
+sudo apt-add-repository -y https://dl.winehq.org/wine-builds/ubuntu/ 
+sudo sed -i -e 's/scope = 1/scope = 0/g' /etc/sysctl.d/10-ptrace.conf
 
-sudo rm -f discord.deb
+# Other repos 
+
+sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer 
+sudo add-apt-repository -y ppa:notepadqq-team/notepadqq 
+
+# More updates 
 
 sudo apt update 
-
 sudo apt upgrade -y 
-
-sudo apt dist-upgrade -y 
-
 sudo apt full-upgrade -y 
+
+# For various reasons, these packages should be installed first 
 
 sudo apt install -y apt-transport-https 
-
+sudo apt install -y openssh-server 
 sudo apt install -y vlc 
 
-sudo apt install -y nodejs 
+# Misc libraries, I recommend keeping everything here 
 
-sudo apt install -y screenfetch 
-
-sudo apt install -y htop 
-
-sudo apt install -y gksu 
-
-sudo apt install -y openssh-server 
-
-sudo apt install -y libtcmalloc-minimal4 
-
-sudo apt install -y libdbusmenu-gtk4 
-
-sudo apt install -y libdbusmenu-gtk4:i386 
-
-sudo apt install -y lib32z1 
-
-sudo apt install -y lib32ncurses5 
-
-sudo apt install -y lib32stdc++6 
-
-sudo apt install -y libopenal1 
-
-sudo apt install -y libgtk* 
-
-sudo apt install -y libsdl2* 
-
-sudo apt install -y libsdl-ttf* 
-
-sudo apt install -y liblua5* 
-
-sudo apt install -y libcap2-bin 
-
-sudo apt install -y vulkan-utils 
-
-sudo apt install -y python-gpgme 
-
-sudo apt install -y mono-complete 
-
-sudo apt install -y mono-vbnc 
-
+sudo apt install -y curl
+sudo apt install -y exfat-* 
 sudo apt install -y flac 
-
+sudo apt install -y lame 
+sudo apt install -y lib32z1
+sudo apt install -y lib32ncurses5 
+sudo apt install -y lib32stdc++6 
+sudo apt install -y libappindicator1 
+sudo apt install -y libc++1 
+sudo apt install -y libcap2-bin 
+sudo apt install -y libdbusmenu-gtk4 
+sudo apt install -y libdbusmenu-gtk4:i386 # Steam
+sudo apt install -y libgnome-keyring-common
+sudo apt install -y libgnome-keyring-dev
+sudo apt install -y liblua5* 
+sudo apt install -y libopenal1 
+sudo apt install -y libsdl2* 
+sudo apt install -y libsdl-ttf* 
+sudo apt install -y libtcmalloc-minimal4 
+sudo apt install -y mono-complete 
+sudo apt install -y nodejs 
+sudo apt install -y openjdk-8-jre 
+sudo apt install -y vulkan-utils 
 sudo apt install -y x264 
 
-sudo apt install -y lame 
-
-sudo apt install -y cmake 
+# Developer tools 
 
 sudo apt install -y build-essential 
-
-sudo apt install -y exfat-* 
-
+sudo apt install -y cmake 
 sudo apt install -y git 
 
-sudo apt install -y aptitude 
+# GitKraken stuff 
 
-sudo apt install -y smartmontools 
+wget -O gitkraken.deb "https://release.gitkraken.com/linux/gitkraken-amd64.deb" 
+sudo dpkg -i gitkraken.deb
+sudo rm -f gitkraken.deb
 
-sudo apt install -y gsmartcontrol 
+# Discord stuff 
 
-sudo apt install -y stress 
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" 
+sudo dpkg -i discord.deb
+sudo rm -f discord.deb
+
+# Misc useful terminal stuff 
 
 sudo apt install -y cmatrix 
-
-sudo apt install -y pv 
-
 sudo apt install -y cowsay 
-
-sudo apt install -y fortune-mod 
-
-sudo apt install -y openjdk-8-jre 
-
-sudo apt install -y ubuntu-sso-client-qt 
-
-sudo apt install -y software-center 
-
-sudo apt install -y synaptic 
-
-sudo apt install -y grub-customizer 
-
-sudo apt install -y gparted 
-
-sudo apt install -y icoutils 
-
-sudo apt install -y dconf-editor 
-
-sudo apt install -y mediainfo 
-
 sudo apt install -y ffmpeg 
-
+sudo apt install -y fortune-mod 
+sudo apt install -y htop 
+sudo apt install -y icoutils 
+sudo apt install -y mediainfo 
+sudo apt install -y neofetch 
+sudo apt install -y pv 
 sudo apt install -y youtube-dl 
 
-sudo apt install -y gnome-system-monitor 
+# Small useful GUI programs 
 
 sudo apt install -y baobab 
-
-sudo apt install -y psensor 
-
+sudo apt install -y dconf-editor 
 sudo apt install -y engrampa 
-
-sudo apt install -y nautilus 
-
+sudo apt install -y gnome-system-monitor 
+sudo apt install -y gparted 
+sudo apt install -y grub-customizer 
+sudo apt install -y psensor 
 sudo apt install -y pcmanfm 
-
-sudo apt install -y gedit 
-
 sudo apt install -y notepadqq 
-
 sudo apt install -y qbittorrent 
 
-sudo apt install -y firefox 
+# Large useful GUI programs 
 
 sudo apt install -y chromium-browser 
-
+sudo apt install -y firefox 
 sudo apt install -y gimp 
-
 sudo apt install -y libreoffice 
 
-sudo apt install -y --install-recommends winehq-devel 
+# Games and other things that can be installed last 
 
+sudo apt install -y winehq-devel --install-recommends 
 sudo apt install -y playonlinux 
-
-sudo update-ca-certificates -f 
-
-sudo setcap cap_sys_ptrace=eip /usr/bin/wineserver 
-
-sudo setcap cap_sys_ptrace=eip /usr/bin/wine-preloader 
-
+sudo apt install -y itch 
 sudo apt install -y libdvd-pkg 
-
 sudo apt install -y steam 
 
-sudo apt install -y itch 
+# Finish with updates, this time update certs too 
 
+sudo update-ca-certificates -f 
 sudo apt update 
-
 sudo apt upgrade -y 
-
-sudo apt dist-upgrade -y 
-
 sudo apt full-upgrade -y 
-
-sudo rm -R /var/log/* 
-
-sudo mkdir /var/log/apt 
 
 echo " " 
 echo "Successfully installed the programs! " 
