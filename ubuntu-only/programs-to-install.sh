5741f9e0c90295d3ee93bf08b17f2a405458afa5
Aaron Franke <arnfranke@yahoo.com>
2018-07-22 23:29:44 -0700

    Add Mono repositories, add warning/note

--- ubuntu-only/programs-to-install.sh
+++ ubuntu-only/programs-to-install.sh
@@ -1,13 +1,31 @@
 
 #!/bin/bash 
 
-sudo dpkg --add-architecture i386 
+echo " " 
+echo "Note: This script is designed for new installs of Ubuntu 18.04 and flavors. " 
+echo "If you already have a system with lots of custom repos, things may conflict. " 
+echo "This script will execute in a few seconds unless you press Ctrl+Z to exit. " 
+echo " " 
+sudo sleep 10 
 
-# Do updates first 
+# Start with updates 
 
 sudo apt update 
 sudo apt upgrade -y 
 sudo apt full-upgrade -y 
+
+# Important stuff that should be done first 
+
+sudo dpkg --add-architecture i386 
+sudo apt install -y apt-transport-https 
+sudo apt install -y dirmngr 
+
+# Mono stuff 
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF 
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF 
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list 
 
 # Itch stuff 
 
@@ -28,22 +46,22 @@ sudo sed -i -e 's/scope = 1/scope = 0/g' /etc/sysctl.d/10-ptrace.conf
 sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer 
 sudo add-apt-repository -y ppa:notepadqq-team/notepadqq 
 
+# Get rid of all existing Snap apps. WARNING: This is meant for new installs only! 
+
+sudo apt purge -y snapd 
+sudo apt install -y snapd 
+
 # More updates 
 
 sudo apt update 
 sudo apt upgrade -y 
 sudo apt full-upgrade -y 
 
-# Get rid of all existing Snap apps. WARNING: This is meant for new installs only! 
 
-sudo apt purge -y snapd 
-sudo apt install -y snapd 
+# These packages should be installed first 
 
-# For various reasons, these packages should be installed first 
-
-sudo apt install -y apt-transport-https 
 sudo apt install -y openssh-server 
-sudo apt install -y vlc 
+sudo apt install -y vlc # Depends on a lot of *specific versions* of libraries 
 
 # Misc libraries, I recommend keeping everything here 
 
@@ -60,21 +78,21 @@ sudo apt install -y libappindicator1
 sudo apt install -y libc++1 
 sudo apt install -y libcap2-bin 
 sudo apt install -y libdbusmenu-gtk4 
-sudo apt install -y libdbusmenu-gtk4:i386 # Steam
+sudo apt install -y libdbusmenu-gtk4:i386 # Steam 
 sudo apt install -y libgnome-keyring-common
 sudo apt install -y libgnome-keyring-dev
 sudo apt install -y liblua5* 
 sudo apt install -y libopenal1 
 sudo apt install -y libsdl2* 
 sudo apt install -y libsdl-ttf* 
-sudo apt install -y libtcmalloc-minimal4 
+sudo apt install -y libtcmalloc-minimal4 # Portal 2 
 sudo apt install -y mono-complete 
 sudo apt install -y nodejs 
 sudo apt install -y openjdk-8-jre 
 sudo apt install -y vulkan-utils 
 sudo apt install -y x264 
 
-# Developer tools 
+# Developer tools, useful for everyone, ex: for compiling software 
 
 sudo apt install -y build-essential 
 sudo apt install -y cmake 
