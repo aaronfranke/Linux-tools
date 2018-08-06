543f440058e1925c9ae80a0e2d23ca3f1fc85c3d
Aaron Franke <arnfranke@yahoo.com>
2018-08-06 16:46:20 -0500

    Remove duplicate Mono line

--- ubuntu-only/programs-to-install.sh
+++ ubuntu-only/programs-to-install.sh
@@ -24,7 +24,6 @@ sudo apt install -y dirmngr # Mono needs it
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF 
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF 
 echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list 
 
 # Itch stuff 
